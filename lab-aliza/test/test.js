9a23643490d9ff7039e2eb002bc6edcb8adf0053
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-21 20:36:13 -0700

    working on testing, cannot figure out how to deal with nickname problem

--- lab-aliza/test/test.js
+++ lab-aliza/test/test.js
@@ -5,44 +5,38 @@ const expect = require('chai').expect;
 const server = require('../lib/_server');
 const port = 5000;
 
-describe('chat server', () => {
-  let client1 = net.connect({port});
-  let client2 = net.connect({port});
-  let messages = ['test', 'Welcome to the chatroom!\n'];
-  let testMessage = ['test'];
 
+
+
+describe('chat server', () => {
   before((done) => {
-    server.listen(port, done);
+    server.listen(port);
+    done();
   });
   after((done) => {
-    server.close(done);
+    server.close();
+    done();
   });
 
-  it('should send messages between clients', (done) => {
+  it('should send data between clients', (done) => {
+    let client1 = net.connect({port});
+    let client2 = net.connect({port});
+    var messages = ['test message 2', 'test message 1', 'Welcome to the chatroom!\n'];
+    var toSend = ['test message 2', 'test message 1'];
+
     client2.on('data', function(data) {
       expect(data.toString()).to.eql(messages.pop());
-      if (testMessage.length > 0){
-        client1.write(testMessage.pop().slice(7));
-      }
-      else {
+      if (toSend.length) {
+        client1.write(toSend.pop());
+      } else {
         client1.end();
       }
     });
-    done();
-  });
 
-  it('should close on "close"', (done) => {
     client1.on('close', function() {
       client2.end();
       expect(messages.length).to.eql(0);
+      done();
     });
-    done();
-  });
-
-  it('should throw err', (done) => {
-    client1.on('error', (err) => {
-      expect(err).to.eql('error: ' + err);
-    });
-    done();
   });
 });
